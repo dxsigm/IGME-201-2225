6c419b3dd901c511dc916842493c3d696d240648
dschuh <david.schuh@mlb.com>
2023-01-18 12:18:35 -0500

    Session 1-1

---
+++ Week 01/Session 1/FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteColorAndNumber
+{
+    // Class: Program
+    // Author: David Schuh
+    // Purpose: Console Read/Write and Exception-handling exercise
+    // Restrictions: None
+
+    /* this is a multi-line comment
+     this is line #2 */
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
