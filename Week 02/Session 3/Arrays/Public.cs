b66ce360dd639c9a35ea49db483bfeb241da1e7b
dschuh <david.schuh@mlb.com>
2023-01-27 13:56:00 -0500

    Session 2-3

---
+++ Week 02/Session 3/Arrays/Public.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Public
+{
+    static internal class Public
+    {
+        static public string myName;
+    }
+}
