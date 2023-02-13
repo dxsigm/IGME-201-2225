9bab2e38d498c24b962e895f2e158e1c9e58449f
dschuh <david.schuh@mlb.com>
2023-02-13 11:57:09 -0500

    Session 5-1

---
+++ Week 05/Session 1/Factorial/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sNumber = null;
+            int nNumber = 0;
+            int nAnswer = 0;
+
+            do
+            {
+                Console.Write("Enter a positive integer: ");
+                sNumber = Console.ReadLine();
+            } while (!int.TryParse(sNumber, out nNumber) && nNumber <= 0);
+
+            nAnswer = 1;
+
+            while( nNumber > 0)
+            {
+                nAnswer *= nNumber;
+                --nNumber;
+            }
+
+
+        }
+
+        static int Factorial(int v)
+        {
+            int returnVal;
+
+            returnVal = v * Factorial(v-1);
+
+            return (returnVal);
+        }
+    }
+}
---
+++ Week 05/Session 1/SortedList/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> dPersonAge = new Dictionary<string,int>();
+
+            dPersonAge["david"] = 50;
+
+            // trying to add 2 items with the same key will generate a runtime exception
+            dPersonAge.Add("david", 65);
+
+            // you can update existing values in the SortedList by referencing an existing key
+            dPersonAge["david"] = 51;
+
+            foreach (KeyValuePair<string, int> thisPerson in dPersonAge)
+            {
+                Console.WriteLine("person[{0}] = {1}", thisPerson.Key, thisPerson.Value);
+            }
+
+            SortedList<string, int> personAge = new SortedList<string, int>();
+
+            personAge["david"] = 50;
+
+            // trying to add 2 items with the same key will generate a runtime exception
+            personAge.Add("david", 65);
+
+            // you can update existing values in the SortedList by referencing an existing key
+            personAge["david"] = 51;
+
+            personAge.Add("joe", 80);
+
+            // if you try to remove an index that doesn't exist, it will throw a runtime exception
+            personAge.Remove("joe");
+
+            foreach( KeyValuePair<string, int> thisPerson in personAge )
+            {
+                Console.WriteLine("person[{0}] = {1}", thisPerson.Key, thisPerson.Value);
+            }
+
+        }
+    }
+}
