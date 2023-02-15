eb0a1becbf72b9e7f926b21d8e4eaf71394098b1
dschuh <david.schuh@mlb.com>
2023-02-15 11:56:11 -0500

    Session 5-2

---
+++ Week 05/Session 2/Factorial/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sNumber = null;
+            int nNumber = 0;
+            int nAnswer = 0;
+
+            do
+            {
+                Console.Write("Enter a positive integer: ");
+                sNumber = Console.ReadLine();
+            } while (!int.TryParse(sNumber, out nNumber) && nNumber <= 0);
+
+            nAnswer = 1;
+
+            while( nNumber > 0)
+            {
+                nAnswer *= nNumber;
+                --nNumber;
+            }
+
+            nAnswer = Factorial(nNumber);
+
+        }
+
+        static int Factorial(int v)
+        {
+            int returnVal;
+
+            // base case: 0! = 1
+            if( v == 0)
+            {
+                returnVal = 1;
+            }
+            else
+            {
+                returnVal = v * Factorial(v - 1);
+            }
+
+            return (returnVal);
+        }
+
+        //Factorial2 : Factorial(2): 2 * Factorial(1) : waiting at line 46
+        //Factorial1 : Factorial(1): 1 * Factorial(0) : waiting at line 46
+        //Factorial0 : Factorial(0): return 1
+        //
+        //Factorial1 : 1 * 1 : return 1
+        //Factorial2 : 2 * 1 : return 2
+
+    }
+}
---
+++ Week 05/Session 2/FactorialOOP/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialOOP
+{
+    class FactorialClass
+    {
+        public int nResult;
+
+        public FactorialClass( int i )
+        {
+            nResult = Factorial(i);
+        }
+
+        public FactorialClass()
+        {
+        }
+
+        private int Factorial(int v)
+        {
+            int returnVal;
+
+            // base case: 0! = 1
+            if (v == 0)
+            {
+                returnVal = 1;
+            }
+            else
+            {
+                returnVal = v * Factorial(v - 1);
+            }
+
+            return (returnVal);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sNumber = null;
+            int nNumber = 0;
+            int nAnswer = 0;
+
+            do
+            {
+                Console.Write("Enter a positive integer: ");
+                sNumber = Console.ReadLine();
+            } while (!int.TryParse(sNumber, out nNumber) && nNumber <= 0);
+
+            FactorialClass factorial = new FactorialClass(nNumber);
+
+            FactorialClass factorial2 = new FactorialClass();
+
+            Console.Write($"{sNumber + "!"} = {factorial.nResult}");
+        }
+    }
+}
