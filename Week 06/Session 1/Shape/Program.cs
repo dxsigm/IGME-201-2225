2dd1f5e48abcf1a4e928263951c582a856007588
dschuh <david.schuh@mlb.com>
2023-02-20 11:57:08 -0500

    Session 6-1

---
+++ Week 06/Session 1/Shape/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    public interface IDrawObject
+    {
+        void DrawMe();
+    }
+
+    public abstract class Shape : IDrawObject
+    {
+        public const double PI = Math.PI;
+        public double x;
+        protected double y;
+
+        private int myInt;
+
+        public Shape()
+        {
+
+        }
+
+        public Shape(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public virtual double Area()
+        {
+            double returnVal = x * y;
+            return returnVal;
+        }
+
+        public abstract void DrawMe();
+    }
+
+    public class Blood : IDrawObject
+    {
+        public virtual void DrawMe()
+        {
+            // code to draw blood
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(double r) : base(r,0)
+        {
+
+        }
+        public override double Area()
+        {
+            double returnVal = PI * x * x;
+            return returnVal;
+        }
+
+        public override void DrawMe()
+        {
+            /// code to specifically draw a circle
+        }
+    }
+
+    internal class Sphere : Shape
+    {
+        public Sphere(double r) : base(r, 0)
+        {
+
+        }
+        public override double Area()
+        {
+            double returnVal = 4 * PI * x * x;
+            return returnVal;
+        }
+
+        public override void DrawMe()
+        {
+            /// code to specifically draw a sphere
+        }
+    }
+
+    internal class Cylinder : Shape
+    {
+        public Cylinder(double r, double h) : base(r, h)
+        {
+
+        }
+        public override double Area()
+        {
+            double returnVal = 2 * PI * x * x + 2 * PI * x * y; ;
+            return returnVal;
+        }
+
+        public override void DrawMe()
+        {
+            /// code to specifically draw a cylinder
+        }
+    }
+
+    internal class Rectangle : Shape
+    {
+        public Rectangle(double w, double h) : base(w, h)
+        {
+
+        }
+
+        public bool IsSquare
+        {
+            get
+            {
+                if (x == y)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public override void DrawMe()
+        {
+            /// code to specifically draw a rectangle
+        }
+    }
+
+    public class Person
+    {
+        public string name;
+    }
+
+    public static class People
+    {
+        public static SortedList<string,Person> people = new SortedList<string,Person>();
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5.5);
+            Cylinder cylinder = new Cylinder(3, 6);
+            Rectangle rectangle = new Rectangle(1, 2);
+
+
+            Person me = new Person();
+            me.name = "david";
+            People.people.Add(me.name, me);
+
+
+
+
+            Shape shape;
+            Rectangle pRectangle;
+            Object obj;
+
+            pRectangle = rectangle;
+            obj = rectangle;
+
+            // can implicitly set a parent type to a child type
+            shape = rectangle;
+
+            shape.x = 5;
+            
+            shape = circle;
+
+
+            //Shape shape2 = new Shape();
+            //Circle circle2;
+            // cannot implicitly set a child type to a parent type
+            //circle2 = shape2;
+
+
+
+            PrintShapeArea(circle);
+            PrintShapeArea(cylinder);
+
+            Object myObj;
+            object myObj2;
+
+            myObj = circle;
+
+            string myString = null;
+            myObj = myString;
+
+            List<Object> list= new List<Object>();
+            list.Add("dave");
+            list.Add(1);
+            list.Add(circle);
+
+            Blood blood = new Blood();
+
+            DrawMyObject(blood);
+            DrawMyObject(shape);
+
+            DoSomethingWithMyObject(blood);
+            DoSomethingWithMyObject(circle);
+        }
+
+        static void DrawMyObject(IDrawObject drawObject)
+        {
+            drawObject.DrawMe();
+        }
+
+        static void DoSomethingWithMyObject(Object obj)
+        {
+
+        }
+
+        static void PrintShapeArea(Shape shape)
+        {
+            if ( shape.GetType() == typeof( Circle) )
+            {
+                Console.WriteLine("Your shape is a circle.");
+            }
+
+            // if our shape is a rectangle
+            if (shape.GetType() == typeof(Rectangle))
+            {
+                // declare a reference Rectangle variable
+                Rectangle r;
+
+                // explicitly cast the parent shape variable to the child type
+                r = (Rectangle)shape;
+
+                // now we can access the IsSquare property
+                if (r.IsSquare)
+                {
+                    Console.WriteLine("it's a square");
+                }
+                
+            }
+
+            if ( shape is Circle)
+            {
+                Console.WriteLine("Your shape is a circle.");
+            }
+
+            if( shape is IDrawObject)
+            {
+                IDrawObject drawObject = shape;
+                drawObject.DrawMe();
+            }
+
+            Console.WriteLine("The area is: " + shape.Area());
+        }
+    }
+}
---
+++ Week 06/Session 1/Shape/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Shape")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tickets.com")]
+[assembly: AssemblyProduct("Shape")]
+[assembly: AssemblyCopyright("Copyright © Tickets.com 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c05576e5-a651-4ba3-8a5e-90cef48acd86")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
