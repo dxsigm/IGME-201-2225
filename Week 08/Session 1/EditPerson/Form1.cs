eb91c4453d58a2773912832909fed0e79203eaca
dschuh <david.schuh@mlb.com>
2023-03-06 14:24:54 -0500

    Session 8-1

---
+++ Week 08/Session 1/EditPerson/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditPerson
+{
+    public partial class EditPersonForm : Form
+    {
+        public EditPersonForm()
+        {
+            InitializeComponent();
+
+            this.Load += new EventHandler(EditPersonForm__Load);
+        }
+
+
+        private void EditPersonForm__Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditPersonForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
