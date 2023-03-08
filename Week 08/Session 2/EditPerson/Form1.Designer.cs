8c0cafb33e22fd2cfc727f18fa3b3e9b0d89b3b3
dschuh <david.schuh@mlb.com>
2023-03-08 11:54:44 -0500

    Session 8-2

---
+++ Week 08/Session 2/EditPerson/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace EditPerson
+{
+    partial class EditPersonForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.typeVal = new System.Windows.Forms.Label();
+            this.typeComboBox = new System.Windows.Forms.ComboBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.licLabel = new System.Windows.Forms.Label();
+            this.licTextBox = new System.Windows.Forms.TextBox();
+            this.specialtyLabel = new System.Windows.Forms.Label();
+            this.specTextBox = new System.Windows.Forms.TextBox();
+            this.gpaLabel = new System.Windows.Forms.Label();
+            this.gpaTextBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // typeVal
+            // 
+            this.typeVal.Location = new System.Drawing.Point(9, 20);
+            this.typeVal.Name = "typeVal";
+            this.typeVal.Size = new System.Drawing.Size(70, 13);
+            this.typeVal.TabIndex = 0;
+            this.typeVal.Text = "Person type:";
+            // 
+            // typeComboBox
+            // 
+            this.typeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.typeComboBox.FormattingEnabled = true;
+            this.typeComboBox.Items.AddRange(new object[] {
+            "Student",
+            "Teacher"});
+            this.typeComboBox.Location = new System.Drawing.Point(80, 18);
+            this.typeComboBox.MaxDropDownItems = 2;
+            this.typeComboBox.Name = "typeComboBox";
+            this.typeComboBox.Size = new System.Drawing.Size(119, 21);
+            this.typeComboBox.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Location = new System.Drawing.Point(9, 55);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(70, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(80, 53);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(207, 20);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.Location = new System.Drawing.Point(9, 92);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(70, 13);
+            this.emailLabel.TabIndex = 2;
+            this.emailLabel.Text = "Email:";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(80, 89);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(352, 20);
+            this.emailTextBox.TabIndex = 2;
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.Location = new System.Drawing.Point(9, 131);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(70, 13);
+            this.ageLabel.TabIndex = 3;
+            this.ageLabel.Text = "Age:";
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(80, 128);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(60, 20);
+            this.ageTextBox.TabIndex = 3;
+            // 
+            // licLabel
+            // 
+            this.licLabel.Location = new System.Drawing.Point(9, 171);
+            this.licLabel.Name = "licLabel";
+            this.licLabel.Size = new System.Drawing.Size(70, 13);
+            this.licLabel.TabIndex = 4;
+            this.licLabel.Text = "License Id:";
+            // 
+            // licTextBox
+            // 
+            this.licTextBox.Location = new System.Drawing.Point(80, 169);
+            this.licTextBox.Name = "licTextBox";
+            this.licTextBox.Size = new System.Drawing.Size(119, 20);
+            this.licTextBox.TabIndex = 4;
+            // 
+            // specialtyLabel
+            // 
+            this.specialtyLabel.Location = new System.Drawing.Point(9, 210);
+            this.specialtyLabel.Name = "specialtyLabel";
+            this.specialtyLabel.Size = new System.Drawing.Size(70, 13);
+            this.specialtyLabel.TabIndex = 5;
+            this.specialtyLabel.Text = "Specialty:";
+            // 
+            // specTextBox
+            // 
+            this.specTextBox.Location = new System.Drawing.Point(80, 207);
+            this.specTextBox.Name = "specTextBox";
+            this.specTextBox.Size = new System.Drawing.Size(352, 20);
+            this.specTextBox.TabIndex = 5;
+            // 
+            // gpaLabel
+            // 
+            this.gpaLabel.Location = new System.Drawing.Point(9, 210);
+            this.gpaLabel.Name = "gpaLabel";
+            this.gpaLabel.Size = new System.Drawing.Size(60, 13);
+            this.gpaLabel.TabIndex = 6;
+            this.gpaLabel.Text = "GPA:";
+            // 
+            // gpaTextBox
+            // 
+            this.gpaTextBox.Location = new System.Drawing.Point(80, 207);
+            this.gpaTextBox.Name = "gpaTextBox";
+            this.gpaTextBox.Size = new System.Drawing.Size(60, 20);
+            this.gpaTextBox.TabIndex = 5;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(281, 243);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(56, 24);
+            this.okButton.TabIndex = 6;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.CausesValidation = false;
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(376, 243);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(56, 24);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // EditPersonForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(452, 286);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.gpaTextBox);
+            this.Controls.Add(this.gpaLabel);
+            this.Controls.Add(this.specTextBox);
+            this.Controls.Add(this.specialtyLabel);
+            this.Controls.Add(this.licTextBox);
+            this.Controls.Add(this.licLabel);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.typeComboBox);
+            this.Controls.Add(this.typeVal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(462, 319);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(462, 319);
+            this.Name = "EditPersonForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Edit Person";
+            this.Load += new System.EventHandler(this.EditPersonForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label typeVal;
+        private System.Windows.Forms.ComboBox typeComboBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Label licLabel;
+        private System.Windows.Forms.TextBox licTextBox;
+        private System.Windows.Forms.Label specialtyLabel;
+        private System.Windows.Forms.TextBox specTextBox;
+        private System.Windows.Forms.Label gpaLabel;
+        private System.Windows.Forms.TextBox gpaTextBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Week 08/Session 2/EditPerson/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditPerson
+{
+    public partial class EditPersonForm : Form
+    {
+        public EditPersonForm()
+        {
+            InitializeComponent();
+
+            foreach(Control control in this.Controls)
+            {
+                // use Tag property to indicate valid state
+                control.Tag = false;
+            }
+
+            this.okButton.Enabled = false;
+
+            this.nameTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.emailTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.ageTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.licTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.specTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.gpaTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+
+            this.Load += new EventHandler(EditPersonForm__Load);
+        }
+
+        private void TxtBoxEmpty__Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+
+            if( tb.Text.Length == 0)
+            {
+                this.errorProvider1.SetError(tb, "This field cannot be empty.");
+                e.Cancel = true;
+                tb.Tag = false;
+            }
+            else
+            {
+                this.errorProvider1.SetError(tb, null);
+                e.Cancel = false;
+                tb.Tag = true;
+            }
+        }
+
+        private void EditPersonForm__Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditPersonForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
